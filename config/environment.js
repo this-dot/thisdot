27590117dbcf0f5129b71285b430d37358889d2b
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-11-10 14:37:00 -0600

    Site basic styles.

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,11 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    googleFonts: [
+      'Raleway:500,500i,600,600i'
+    ]
   };
 
   if (environment === 'development') {
