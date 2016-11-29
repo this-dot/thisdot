a63f2c5cc0b99ceb7c287ffcbbe3e25b9a7684a2
Taras Mankovski <tarasm@gmail.com>
2016-11-29 18:08:47 -0500

    Refactored to rewritter version of ember-writer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,16 +1,1 @@
-import DS from 'ember-data';
-
-export default DS.JSONAPIAdapter.extend(DS.BuildURLMixin, {
-  namespace: 'api/blog/',
-
-  urlForFindAll(modelName) {
-    let url = [];
-    let prefix = this.urlPrefix();
-    let path = this.pathForType(modelName);
-
-    url.push(prefix);
-    url.push(path);
-
-    return `${url.join('/')}.json`;
-  }
-});
+export { default } from 'ember-writer/adapters/application';
