50b7766bfd8cd2939c9560485dc0ace8e7415f61
Taras Mankovski <tarasm@gmail.com>
2016-12-01 18:02:33 -0500

    Made subscription form on frontend work
    Need help

---
+++ app/components/home-subscribe.js
@@ -0,0 +1,8 @@
+import EmberChimp from 'ember-chimp/components/ember-chimp';
+
+export default EmberChimp.extend({
+  placeholder: "Your Email Here",
+  formAction: "//thisdot.us12.list-manage.com/subscribe/post?u=81e8e3fa2a6f79fe97467029a&amp;id=9f8c1ada95",
+  buttonText: "Join",
+  loadingText: "Loading",
+});
