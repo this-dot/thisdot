4c4ea3e37832fb153f0a731175f03fef4a9854c9
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-12-15 10:42:36 -0600

    Add this.javascript to media

--- app/components/video-card.js
+++ app/components/video-card.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
+import moment from 'moment';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
-  classNames: ['video-card']
+  classNames: ['video-card'],
+  classNameBindings: ['isFuture'],
+  isFuture: computed('post.date', function() {
+    let today = moment();
+    let eventDate = moment(this.get('post.date'));
+    return eventDate.isAfter(today);
+  })
 });
