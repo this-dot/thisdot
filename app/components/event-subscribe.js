2f748c3cb333b203e7997e952eea7a5724189232
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2017-01-17 18:18:42 -0600

    Fix subscribe forms for events

--- app/components/event-subscribe.js
+++ app/components/event-subscribe.js
@@ -1,10 +1,26 @@
 import Ember from 'ember';
 import EmberChimp from 'ember-chimp/components/ember-chimp';
-const { computed } = Ember;
+const { computed, get, isPresent } = Ember;
 
 export default EmberChimp.extend({
   formAction: "//thisdot.us12.list-manage.com/subscribe/post?u=81e8e3fa2a6f79fe97467029a&amp;id=9f8c1ada95",
   buttonText: "Register",
   loadingText: "Loading",
-  isSuccess: computed.equal('chimpState', 'success')
+  isSuccess: computed.equal('chimpState', 'success'),
+
+  handleResponse(response) {
+    this._super(response);
+    let errorMsg = get(response, 'msg');
+    if (response.result === 'error' && isPresent(errorMsg)) {
+      this.set('chimpSays', errorMsg);
+    }
+  },
+
+  didUpdateAttrs() {
+    this._super(...arguments);
+    this.setProperties({
+      chimpSays: '',
+      chimpState: ''
+    });
+  }
 });
