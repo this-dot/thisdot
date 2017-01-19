d8c12bce7c3229de3b054bced550b31914372637
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2017-01-19 14:54:41 -0600

    Consulting styles and markup

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
+const { inject: { service } } = Ember;
 
 export default Ember.Component.extend({
+  navState: service(),
+
   tagName: 'header',
-  classNames: ['nav-bar']
+  classNames: ['nav-bar'],
+  classNameBindings: ['navState.hideLogo']
 });
