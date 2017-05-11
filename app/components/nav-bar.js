08a1b162ccd3ed56874149df4e4de94024ca18dd
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2017-05-11 15:59:07 -0600

    New header styles

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
-const { computed: { readOnly }, inject: { service } } = Ember;
+const { computed: { readOnly, match, or, not }, inject: { service } } = Ember;
 
 export default Ember.Component.extend({
   tagName: 'header',
   classNames: ['nav-bar'],
   router: service('-routing'),
-  activeRoute: readOnly('router.route')
+  activeRoute: readOnly('router.currentRouteName'),
+
+  isMedia: match('activeRoute', /media.*\W*/),
+  isLabs: match('activeRoute', /labs.*\W*/),
+
+  isMediaOrLabs: or('isMedia', 'isLabs'),
+  isOther: not('isMediaOrLabs')
 });
