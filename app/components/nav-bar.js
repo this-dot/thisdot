09dda2327f86c4c544414eaf5d120ab6f685ff97
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2017-05-11 14:19:00 -0600

    Media styles

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+const { computed: { readOnly }, inject: { service } } = Ember;
 
 export default Ember.Component.extend({
   tagName: 'header',
-  classNames: ['nav-bar']
+  classNames: ['nav-bar'],
+  router: service('-routing'),
+  activeRoute: readOnly('router.route')
 });
