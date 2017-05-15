ba3c2d753034fc74a3b37ec281c869a8fc2a9ce4
Taras Mankovski <tarasm@gmail.com>
2017-05-15 08:31:33 -0400

    Removed a bunch of unnecessary templates and routes

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,11 +3,6 @@ const { inject: { service } } = Ember;
 
 export default Ember.Route.extend({
   headData: service(),
-  model() {
-    return {
-      featured: this.store.findRecord('post', 'mw-thinking-reactively-with-rx-js-5')
-    };
-  },
   afterModel() {
     this.set('headData.title', 'This Dot: the evolution of the modern web');
   }
