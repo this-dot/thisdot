b4762ca236c2bdf3ea7737b20a3c491748bc8584
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2017-06-08 13:51:13 -0600

    Rever frontpage

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,6 +3,11 @@ const { inject: { service } } = Ember;
 
 export default Ember.Route.extend({
   headData: service(),
+  model() {
+    return {
+      featured: this.store.findRecord('post', 'mw-thinking-reactively-with-rx-js-5')
+    };
+  },
   afterModel() {
     this.set('headData.title', 'This Dot: the evolution of the modern web');
   }
