b59a31e4b0eef9882e11423d85402cacc3c18cae
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2017-06-16 12:02:47 -0600

    Fixed About/Team CSS issues

--- app/routes/about.js
+++ app/routes/about.js
@@ -4,7 +4,53 @@ import mentors from 'thisdot/utils/mentors';
 export default Ember.Route.extend({
   model() {
     return {
-      mentors
+      mentors,
+      founders: [
+        {
+          name: 'Taras Mankovski',
+          image: '/media/mentors/taras-mankovski.jpg',
+          twitter: 'tarasm',
+          email: 'taras@thisdot.co'
+        },
+        {
+          name: 'Tracy Lee',
+          image: '/media/mentors/tracy-lee.jpg',
+          twitter: 'ladyleet',
+          email: 'tracy@thisdot.co'
+        },
+        {
+          name: 'Ben Lesh',
+          image: '/media/mentors/ben-lesh.png',
+          twitter: 'BenLesh',
+          email: 'ben@thisdot.co'
+        },
+        {
+          name: 'Mike Auty',
+          image: '/media/mentors/mike-auty.jpg',
+          twitter: 'mauty',
+          email: 'mike@thisdot.co'
+        }
+      ],
+      team: [
+        {
+          name: 'Wentao Shu',
+          image: '/media/team/wentao_shu.jpg',
+          title: 'Operations Manager',
+          email: 'wentao@thisdot.co'
+        },
+        {
+          name: 'Sarah Rech',
+          image: '/media/team/sarah_rech.jpg',
+          title: 'Marketing & Operations',
+          email: 'sarah@thisdot.co'
+        },
+        {
+          name: 'Jorge Lainfiesta',
+          image: '/media/team/jorge_lainfiesta.jpg',
+          title: 'Resident Mentor',
+          email: 'jorge@thisdot.co'
+        }
+      ]
     };
   }
 });
